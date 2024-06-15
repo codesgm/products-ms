b28c29061c44108111bc5b31a15a2691ed5e19ea
Guilherme Mello <guilhermetts0@gmail.com>
2024-06-15 12:57:33 -0300

    first commit

---
+++ src/test/java/com/products/products_ms/ProductsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.products.products_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
