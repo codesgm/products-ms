10d587249f38ec168c2775906ef6476713d6c81e
Guilherme Mello <guilhermetts0@gmail.com>
2024-06-16 17:56:33 -0300

    add func findProductById

--- src/main/java/com/products/products_ms/resources/UserResource.java
+++ src/main/java/com/products/products_ms/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.products.products_ms.resources;
 
 import com.products.products_ms.entities.User;
+import com.products.products_ms.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
-
+    @Autowired
+    private UserService userService;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-     User u = new User(1L,"John","LrSjM@example.com","123456789","123456");
-     return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+     return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity <User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return  ResponseEntity.ok().body(obj);
+    }
+
 
 }
